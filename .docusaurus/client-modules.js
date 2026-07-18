ed4bbcde55da2ac54189654b9c5c1ae2ec42dc12
Charan <charan@Charans-MacBook-Pro.local>
2026-07-18 19:01:01 +0400

    project cleanuo

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/charan/Documents/techblogging/perpetualengineer/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/charan/Documents/techblogging/perpetualengineer/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/charan/Documents/techblogging/perpetualengineer/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/charan/Documents/techblogging/perpetualengineer/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/charan/Documents/techblogging/perpetualengineer/src/css/custom.css"),
-];
